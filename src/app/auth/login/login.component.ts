592dd78dd1993ce6b0355e0a5f21c050569632fb
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-07-31 08:07:07 +0300

    Authorizations Finished

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  isLoading = false;
+  constructor(private authService : AuthService) {}
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  onLogin() {
+    if(this.loginForm.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.authService.login(this.loginForm.value.email, this.loginForm.value.password);
+  }
+
+}
---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
+  isLoading = false;
+  constructor(private authService : AuthService) {}
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSignup() {
+    if (this.signupForm.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    this.authService.signup(this.signupForm.value.email, this.signupForm.value.password);
+  }
+}
